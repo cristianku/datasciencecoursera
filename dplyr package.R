5dc8f52ea2c400d8aa78df62032c2b2c445d49c8
cristianku <c.zantedeschi@gmail.com>
2017-02-23 06:40:20 -0800

    aaa

--- dplyr package.R
+++ dplyr package.R
@@ -54,6 +54,27 @@ summarize ( hotcold, pm25= mean(pm25, na.rm=TRUE), o3= max(o3tmean2), no2 = medi
 #   3      NA 47.73750  9.416667 37.44444
 
 
+chicago %>% mutate(month= as.POSIXlt(date)$mon + 1 ) %>% summarize (pm25 = mean(pm25,na.rm=TRUE), o3 = max(o3tmean2), no2 = median(no2tmean2))
 
+#pm25      o3      no2
+#1 16.23096 66.5875 24.55556
+
+# group by month
+chicago %>% mutate(month= as.POSIXlt(date)$mon + 1 ) %>% group_by(month) %>%summarize (pm25 = mean(pm25,na.rm=TRUE), o3 = max(o3tmean2), no2 = median(no2tmean2))
+
+# month     pm25       o3      no2
+# <dbl>    <dbl>    <dbl>    <dbl>
+# 1      1 17.76996 28.22222 25.35417
+# 2      2 20.37513 37.37500 26.78034
+# 3      3 17.40818 39.05000 26.76984
+# 4      4 13.85879 47.94907 25.03125
+# 5      5 14.07420 52.75000 24.22222
+# 6      6 15.86461 66.58750 25.01140
+# 7      7 16.57087 59.54167 22.38442
+# 8      8 16.93380 53.96701 22.98333
+# 9      9 15.91279 57.48864 24.47917 
+# 10    10 14.23557 47.09275 24.15217
+# 11    11 15.15794 29.45833 23.56537
+# 12    12 17.52221 27.70833 24.45773
 
 
