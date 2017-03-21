f83b6ca0de39edde5a6029811d5497dc40160300
cristianku <c.zantedeschi@gmail.com>
2017-03-21 00:56:57 -0700

    aa

--- plotting with R.R
+++ plotting with R.R
@@ -25,4 +25,28 @@ boxplot(Ozone ~ Month, airquality , xlab = "Month", ylab = "Ozone (ppb ) ")
 
 #pch = the plotting sympol ( default is open circle)
 
-#lty
+#lty = the line type ( default is solid line ), can be dashed, dotted, etc..
+
+#lwd = the line width , specified as an integer multiple
+
+#col = the plotting, specified as a number , string, or hex code: the colors() function gives you a vector of colors by name
+
+#xlab = character string for the x-axis label
+
+#ylab = character string for the y-axis label
+
+# global plotting parameters
+
+# the par() is used to specify global graphics parameters
+
+#las = the orientation of x-axis on the plot
+#bg  = the background color
+#mar = the margin size
+#oma = the outer margin size ( default is 0 for all sides )
+#mfrow= number of plots per row , column ( plots are filled row-wise)
+#mfcol = number of plots  per row , column ( plots are filled column-wise)
+
+
+
+
+
