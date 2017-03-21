068ddf321a3b0d8518f96382a5837c5fc61c9f33
cristianku <c.zantedeschi@gmail.com>
2017-03-21 08:33:14 +0100

    aa

---
+++ reading USA pm 2.5.R
@@ -0,0 +1,21 @@
+pollution <- read.csv("./data/avgpm25.csv")
+head(pollution)
+
+# base plot
+
+library(datasets)
+with ( cars, plot ( speed, dist ))
+
+# LATTICE PLOT
+library(lattice)
+state <- data.frame(state.x77, region = state.region)
+xyplot(Life.Exp ~ Income | region, data = state, layout = c(4,1))
+
+# gg plot 2
+library(ggplot2)
+qplot(displ, hwy, data= mpg)
+
+# simple basic graphis : boxplot
+library (datasets)
+airquality <- transform(airquality, Month=factor(Month))
+boxplot(Ozone ~ Month, airquality , xlab = "Month", ylab = "Ozone (ppb ) ")
